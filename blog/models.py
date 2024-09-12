a7eece48df7154298be5f479cbc5c07bc096f158
Patrick Galvin <patjoegalvin@gmail.com>
2024-09-12 09:58:42 +0000

    Add: more users and posts

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,12 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User,
+        on_delete=models.CASCADE,
+        related_name="blog_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
