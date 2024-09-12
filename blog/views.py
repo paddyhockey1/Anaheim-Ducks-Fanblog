dd5cebab9fc36eb1d9eb9313b18807a5c1e16a12
Patrick Galvin <patjoegalvin@gmail.com>
2024-09-12 10:12:05 +0000

    Add: post list on home page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
-def my_blog(request):
-    return HttpResponse("Anaheim Ducks Blog")
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
