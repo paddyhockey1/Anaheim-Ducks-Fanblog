99a490f9a813f1ca524a9d03dd29bdf89c68aad3
Patrick Galvin <patjoegalvin@gmail.com>
2024-09-16 07:42:23 +0000

    Fix: About Page

--- about/models.py
+++ about/models.py
@@ -1,8 +1,12 @@
-from django.contrib import admin
-from .models import About
-from django_summernote.admin import SummernoteModelAdmin
+from django.db import models
+
+# Create your models here.
 
 
-@admin.register(About)
-class AboutAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content',)
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
