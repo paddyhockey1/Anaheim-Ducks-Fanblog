eff2216292a3080b1353573895e1f4a7b7ce81f6
Patrick Galvin <patjoegalvin@gmail.com>
2024-09-16 08:48:49 +0000

    Remove: about folder

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
 ]
