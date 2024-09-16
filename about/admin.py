44be434159ffc6c7740588b2f56b2500854d5889
Patrick Galvin <patjoegalvin@gmail.com>
2024-09-16 08:25:24 +0000

    Change style.css pages

--- about/admin.py
+++ about/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import About
 from django_summernote.admin import SummernoteModelAdmin
+from .models import About, CollaborateRequest
 
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
+    """
+    Adds rich-text editing of content in admin
+    """
     summernote_fields = ('content',)
+
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+    """
+    Lists message and read fields for display in admin
+    """
+    list_display = ('message', 'read',)
--- about/urls.py
+++ about/urls.py
@@ -1,5 +1,7 @@
-from . import views
+
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
     path('', views.about_me, name='about'),
