348cae1abf62a32bd1c82c5abd0e7639cd9365e3
Patrick Galvin <patjoegalvin@gmail.com>
2024-09-18 11:21:23 +0000

    Fix: Profile Issue

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
+    path("profile/",  include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
 ]
--- profile/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import profile
-
-urlpatterns = [
-    # Add this
-    path('profile/', profile, name='users-profile'),
-]
