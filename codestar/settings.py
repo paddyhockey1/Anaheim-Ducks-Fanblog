9b0e6e516a07ccb441ab89e5e59faa4e64e1054a
Patrick Galvin <patjoegalvin@gmail.com>
2024-09-19 13:50:30 +0000

    Change: Login/Logout redirect urls

--- codestar/settings.py
+++ codestar/settings.py
@@ -57,8 +57,8 @@ INSTALLED_APPS = [
 ]
 
 SITE_ID = 1
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'https://8000-paddyhockey-anaheimduck-omqufb8p97x.ws-eu116.gitpod.io/'
+LOGOUT_REDIRECT_URL = 'https://8000-paddyhockey-anaheimduck-omqufb8p97x.ws-eu116.gitpod.io/admin/login/?next=/admin/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
