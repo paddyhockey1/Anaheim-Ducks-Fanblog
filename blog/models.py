f8cceade477dfc2e04328fe5448b3eb2e41ee824
Patrick Galvin <patjoegalvin@gmail.com>
2024-09-23 13:59:37 +0000

    Add: Extra Post

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
@@ -26,8 +26,10 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 
+    
+
 class Meta:
-    ordering = ["-created_on"]
+    ordering = ["-created_date"]
 
 
 def __str__(self):
