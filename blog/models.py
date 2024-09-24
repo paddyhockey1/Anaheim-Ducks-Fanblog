f5647af512bac1510846d2c526206310c832b1b5
Patrick Galvin <patjoegalvin@gmail.com>
2024-09-24 07:36:42 +0000

    Create new Procfile

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
@@ -26,10 +26,8 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 
-    
-
 class Meta:
-    ordering = ["-created_date"]
+    ordering = ["-created_on"]
 
 
 def __str__(self):
